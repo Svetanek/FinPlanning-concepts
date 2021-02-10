a919f2ed25cb810c2c983f8ff97c0ba1dbf917b5
Svetlana Shinkar <svetanek@gmail.com>
2021-02-09 21:12:35 -0500

    changed to neumorphism style

--- src/components/form-input/form-input.js
+++ src/components/form-input/form-input.js
@@ -4,7 +4,7 @@ import './form-input.styles.scss';
 const FormInput = ({children, handleChange, htmlFor, currency, percent, ...otherProps}) => (
   <div className="fieldsetLine">
   <label className="fieldsetLine_label">{children}</label>
-  <div className="fieldsetLine_inputWrap">{currency? <span>$</span> : null}{percent? <span>%</span> : null } <input  className="fieldsetLine_input"
+  <div className="fieldsetLine_inputWrap">{currency? <span>$</span> : null}{percent? <span>%</span> : null } <input  className="form-fieldsetLine_input"
       onChange={handleChange}
       {...otherProps} />
       </div>
--- src/global-styles.js
+++ src/global-styles.js
@@ -1,15 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
-//background-color: #83ced4 #0c899b;
- // background-color: #c1fce8;
-// background-image: linear-gradient(to bottom, #a8d4e6, #73a9bf);
 const GlobalStyle = createGlobalStyle`
 body {
   font-family: 'Lora';
   font-weight: 400;
   font-size: 16px;
   min-height: 100vh;
-  background-image: linear-gradient(to bottom, #bce4e9, #77bcc5);;
+  background-image: linear-gradient(to bottom, #d4d4d48a, #aad3da);
   padding: 1rem;
   @media screen and (max-width: 900px) {
   padding: 0.625em;
